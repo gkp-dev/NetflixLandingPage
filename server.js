e8a9832eeb949242757ec728dc762b762e00bf59
gkp-dev <gkprofessionel@gmail.com>
2021-03-05 16:00:17 +0100

    update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('./public'));
 
-const port = process.env.PORT = 2300;
+const port = process.env.PORT || 2300;
 
 app.listen(port, () => {
     console.log(`My app is runnning on port ${port}`)
